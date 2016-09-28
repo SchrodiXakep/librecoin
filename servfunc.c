a0b62a977299fbe17ef1250bc2d29746adf6147e
Schrodi <Schrodi@riseup.net>
2016-09-27 21:05:27 -0700

    git add crap.

---
+++ servfunc.c
@@ -0,0 +1,13 @@
+/* Copyright (C) 2015 Schrodi */
+/********************************************************
+ * servfunc -- functions to start listening.            *
+ ********************************************************/
+
+/* Copyright (C) 1991-2014 Free Software Foundation, Inc. */
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <string.h>
+
+/* Function Prototypes */
+void start_listening(){};
---
+++ servfunc.h
@@ -0,0 +1,9 @@
+/* Copyright (C) 2015 Schrodi */
+
+#ifndef __SERVFUNC__
+#define __SERVFUNC__
+
+void start_listening(void);
+
+// End __SERVFUNC__ ifndef
+#endif
